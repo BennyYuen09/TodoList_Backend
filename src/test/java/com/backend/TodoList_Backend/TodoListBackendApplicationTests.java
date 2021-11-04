2fb7e9c816aec099a46af00f0999e7be4e662b5a
BennyYuen09 <Benny.Yuen@agileexlab.net>
2021-11-04 11:06:31 +0800

    Initial commit

---
+++ src/test/java/com/backend/TodoList_Backend/TodoListBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.TodoList_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
